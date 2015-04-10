01d3362471726eca67b57d647618c7a546430721
Arpit1286 <arpit.tandon@ymail.com>
2015-04-10 13:08:07 -0700

    Pair Count Added

--- src/main/scala/TextPairCount.scala
+++ src/main/scala/TextPairCount.scala
@@ -10,9 +10,10 @@ object TextPairCount {
     val conf = new SparkConf().setAppName("TextPair")
     val sc = new SparkContext(conf)
 
-    val input = sc.textFile("/path/to/pair.txt")  // add command line options instead of hard coded path
-    val textPairRDD = input.map(x => (x.split(" ")(0), x.split(" ")(1)))
-    val pairCount = textPairRDD.map(x => (x,1)).reduceByKey(_ + _)
+    val input = sc.textFile("/path/to/pair.txt")
+    val textPairRDD = input.map(x => (x.split(" ")(0), x.split(" ")(1)))  // make them into a tuple
+    val pairCount = textPairRDD.map(x => (x,1)).reduceByKey(_ + _)  // collect and aggregate
+    pairCount.saveAsTextFile("/path/to/output.txt")
   }
 }
 
