542737567fe2faa79b95ab10e58088fb3a12f499
Arpit1286 <arpit.tandon@ymail.com>
2015-04-10 12:58:24 -0700

    updated SBT with streaming dependency

--- src/main/scala/TextPairCount.scala
+++ src/main/scala/TextPairCount.scala
@@ -1,17 +1,18 @@
-import
+import org.apache.spark.{SparkContext, SparkConf}
 
-
-class TextPair(var first: String, var second: String) {
-
-
-
-
-
-}
+// this gives seriliazation exception, look deep into it
+//class TextPair(var first: String, var second: String) {
+//  val returnTuple = (first, second)
+//}
 
 object TextPairCount {
   def main(args: Array[String]) {
-    val conf = new SparkConf
+    val conf = new SparkConf().setAppName("TextPair")
+    val sc = new SparkContext(conf)
+
+    val input = sc.textFile("/path/to/pair.txt")
+    val textPairRDD = input.map(x => (x.split(" ")(0), x.split(" ")(1)))
+    val pairCount = textPairRDD.map(x => (x,1)).reduceByKey(_ + _)
   }
 }
 
