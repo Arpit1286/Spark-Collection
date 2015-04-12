ea897476aa19dd18c0b12f8dabfcdc3c2d805688
Arpit1286 <arpit.tandon@ymail.com>
2015-04-11 17:13:24 -0700

    Using case class for textPairCount

--- src/main/scala/CustomPartitionerForApacheLogs.scala
+++ src/main/scala/CustomPartitionerForApacheLogs.scala
@@ -1,7 +1,12 @@
+import org.apache.spark.{SparkContext, SparkConf}
 
 object CustomPartitionerForApacheLogs {
   def main(args: Array[String]) {
+    val conf = new SparkConf().setAppName()
+    val sc = new SparkContext(conf)
 
+    val logInput = sc.textFile("/path/to/logTextFile") //make it use log file from command line
+    val
   }
 }
 
--- src/main/scala/TextPairCount.scala
+++ src/main/scala/TextPairCount.scala
@@ -5,6 +5,29 @@ import org.apache.spark.{SparkContext, SparkConf}
 //  val returnTuple = (first, second)
 //}  try using 'case class' to see if it works
 
+case class TextPair(first: String, second: String)
+
+object TextPairCount {
+  def main(args: Array[String]) {
+    val conf = new SparkConf().setAppName("TextPairCount")
+    val sc = new SparkContext(conf)
+
+    val input = sc.textFile("/path/to/Inputfile")
+    val textPairs = input.map(line => parse(line))
+    val pairCount = textPairs.map(x => (x,1)).reduceByKey(_ + _)
+    pairCount.saveAsTextFile("/path/to/output")
+  }
+
+  def parse(input: String): TextPair = {
+    val parts = input.split(" ")
+    val first = parts(0)
+    val second = parts(1)
+    TextPair(first, second)
+  }
+}
+
+
+/*
 object TextPairCount {
   def main(args: Array[String]) {
     val conf = new SparkConf().setAppName("TextPair")
@@ -16,4 +39,4 @@ object TextPairCount {
     pairCount.saveAsTextFile("/path/to/output.txt")
   }
 }
-
+*/
