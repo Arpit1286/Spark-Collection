f01fa204bfbc5939977ccc7000af26267923c210
Arpit1286 <arpit.tandon@ymail.com>
2015-04-13 13:14:23 -0700

    added kafka streaming dependency

--- build.sbt
+++ build.sbt
@@ -6,7 +6,8 @@ scalaVersion := "2.10.4"
 
 libraryDependencies ++= Seq(
   "org.apache.spark" %% "spark-core" % "1.3.0",
-  "org.apache.spark" %% "spark-streaming" % "1.3.0"
+  "org.apache.spark" %% "spark-streaming" % "1.3.0",
+  "org.apache.spark" %% "spark-streaming-kafka_2.10" % "1.3.0"
 )
 
 
--- src/main/scala/StreamingExample.scala
+++ src/main/scala/StreamingExample.scala
@@ -1,15 +1,13 @@
-import org.apache.spark.streaming.{Seconds, StreamingContext, Duration}
-import org.apache.spark.streaming.StreamingContext._
-import org.apache.spark.streaming.dstream.DStream
 import org.apache.spark.SparkConf
+import org.apache.spark.streaming.kafka._
+// reading from Kafka and writing into local disk
 
-val conf = new SparkConf()
-val ssc = new StreamingContext(conf, Seconds(1))
-//create a Dstream using data recieved after connecting to port 7777 on local machine
-val lines = ssc.socketTextStream("localhost", 7777)
-// filter our Dstream for lines with "error"
-val errorLines = lines.filter(_.contains("error"))
 
-errorLines.print()
+object StreamingExample {
+  def main(args: Array[String]) {
+    val conf = new SparkConf()
+    val kafkaStream = KafkaUtils.createStream()
+  }
+}
 
 
