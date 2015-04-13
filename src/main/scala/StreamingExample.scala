298c952e1dbe9dd7398a5922c4fa70fbc065c714
Arpit1286 <arpit.tandon@ymail.com>
2015-04-13 16:10:53 -0700

    waiting for kafka server to be up

--- src/main/scala/StreamingExample.scala
+++ src/main/scala/StreamingExample.scala
@@ -1,15 +1,16 @@
 import org.apache.spark.SparkConf
 import org.apache.spark.streaming.{Seconds, StreamingContext}
-import org.apache.spark.streaming.kafka.
+import org.apache.spark.streaming.kafka._
 
 // reading from Kafka and writing to local disk
 
 
 object StreamingExample {
   def main(args: Array[String]) {
+
     val conf = new SparkConf().setAppName("kafkaStreamWriter")
     val ssc = new StreamingContext(conf, Seconds(2))
-    val
+    val kafkaStream = KafkaUtils.createStream(ssc, 3, , 1 )
   }
 }
 
