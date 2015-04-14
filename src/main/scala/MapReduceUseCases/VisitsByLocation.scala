7c1fe3b3b3ccb5b96fb9ed23a6b84d9db1dbdd86
Arpit1286 <arpit.tandon@ymail.com>
2015-04-14 16:07:40 -0700

    Added Visits by location class

--- src/main/scala/MapReduceUseCases/VisitsByLocation.scala
+++ src/main/scala/MapReduceUseCases/VisitsByLocation.scala
@@ -12,7 +12,7 @@ object VisitsByLocation {
   val input = sc.textFile("/path/to/inputLog")
   val IPInput = input.map(line => line.split(" ")(0)) // has Ips as string array
   val locationFromLogRDD = IPInput.map(IPString => geoLookup(IPString)) // this gives us the location as defined in the geolookup
-  // do the wordCount to get the number of visits from each country, city and so on
+  // do the wordCount to get the number of visits from each country, city and so on, complete it after figuring out the next set
 
 
 }
