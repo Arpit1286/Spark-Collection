408523bae632a542774c3464973e58ecd33764f8
Arpit1286 <arpit.tandon@ymail.com>
2015-04-14 14:15:42 -0700

    geolocation lookup method added

--- src/main/scala/GeoLocationLookup.scala
+++ src/main/scala/GeoLocationLookup.scala
@@ -2,24 +2,26 @@ import java.net.InetAddress
 import java.net.InetAddress.getByName
 import com.maxmind.geoip2.WebServiceClient.Builder
 
-case class location(CountryCode: String, Country: String, City: Option[String], region: Option[String], postalCode: Option[String])
+case class location(CountryCode: String, Country: String, City: String, region: String, postalCode: String)
 
 // call the webservice client
 
 val webClient = new Builder(42, "licenseKey").build()
 
+// naive implementation
+object GeoLocationLookup {
 
-class GeoLocationLookup {
-  var IPString = "0.0.0.0"
-  val IPAddress = new InetAddress(getByName(IPString))
-  val insights = webClient.insights(IPAddress)
-  // yet to add the exception cases
-  def geoLookup(IP: String): Unit ={
+  // geolookup method, yet to add exception cases
+  def geoLookup(IP: String): location ={
+    val IPAddress = new InetAddress(getByName(IP))
+    val insights = webClient.insights(IPAddress)
+    // get the country name, City name, Country Code and Zip code as String
     val Country = insights.getCountry.getName
     val City = insights.getCity.getName
     val CountryCode = insights.getCountry.getIsoCode
     val region = insights.getMostSpecificSubdivision.getName
     val zipCode = insights.getPostal.getCode
+    location(CountryCode, Country, City, region, zipCode)
   }
 
 }
