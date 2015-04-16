4d722b9775eabad4edc3e5a7b8d24ea3cc3d4b44
Arpit1286 <arpit.tandon@ymail.com>
2015-04-15 23:34:43 -0700

    HttpLogParserAdded

--- src/main/scala/common/parsers/HttpLogStringParser.scala
+++ src/main/scala/common/parsers/HttpLogStringParser.scala
@@ -2,21 +2,30 @@ package common.parsers
 
 import com.github.nscala_time.time.Imports._
 
-class HttpLogStringParser {
+case class HttpLogStringParser(ip: String,
+                               RFC1413: String,
+                               UserID: String,
+                               TimeStamp: DateTime,
+                               RequestLine: String,
+                               StatusCode: Int,
+                               Size: Double,
+                               Referer: String,
+                               BrowserInfo: String)
 
+
+object HttpLogStringParser {
   // regular expression for the log format used. minor variations in the format would require this to change
   val pattern = """^([\d.]+) (\S+) (\S+) \[(.*)\] \"([^\s]+) (/[^\s]*) HTTP/[^\\s]+\" (\d{3}) (\d+) \"([^\"]+)\" \"([^\"]+)\"$""".r
-
-  def getTimeStamp(input: String): DateTime = {
-
+  def getHttpLogString(logLine: String) = logLine match {
+    case pattern(ip,
+    rfc1413,
+    userID,
+    timeStamp,
+    requestLine,
+    statusCode,
+    size,
+    referer,
+    browserInfo) => Some(HttpLogStringParser(ip, rfc1413, userID, timeStamp.toDateTime, requestLine,statusCode.toInt,size.toDouble,referer,browserInfo))
+    case _ => None
   }
-
-  def getIp(input: String): String = {
-
-  }
-
-  def getRequestPage(input: String): String = {
-
-  }
-
 }
