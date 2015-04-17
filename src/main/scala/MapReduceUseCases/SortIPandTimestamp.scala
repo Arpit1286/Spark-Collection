f5d3223a70e9b0f1d0de592c7aa7c936f82e850d
Arpit1286 <arpit.tandon@ymail.com>
2015-04-16 19:57:51 -0700

    Sort comparator needs to be created

--- src/main/scala/MapReduceUseCases/SortIPandTimestamp.scala
+++ src/main/scala/MapReduceUseCases/SortIPandTimestamp.scala
@@ -1,25 +1,28 @@
 // secondary sort in Spark
 package MapReduceUseCases
 
-import java.text.SimpleDateFormat
-
 import common.parsers.HttpLogStringParser
 import com.github.nscala_time.time.Imports._
-
 import org.apache.spark.{SparkContext, SparkConf}
 
 
 object SortIPandTimestamp {
-  val formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss ")  // date formatter
   val conf = new SparkConf().setAppName("SortByCompositeKey")
   val sc = new SparkContext(conf)
+
+  def createCompositeKey(line: String): compositeKey = {
+    val ip = new HttpLogStringParser(line).getIP
+    val timeStamp = new HttpLogStringParser(line).getTimeStamp
+    compositeKey(ip, timeStamp)
+  }
 
   // composite key sorted by timestamp
   val input = sc.textFile("/path/to/textFile")
   // RDD on composite key
-  val splitRDD = input.map(line => ((new HttpLogStringParser(line).getIP, new HttpLogStringParser(line).getTimeStamp),line))
+  val splitRDD = input.map(line => (createCompositeKey(line),line))
 
 }
+
 
 
 case class compositeKey(IP: String, TimeStamp: DateTime)
