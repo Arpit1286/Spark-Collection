7941166ce608fc7ed62773ce41433ace18331834
Arpit1286 <arpit.tandon@ymail.com>
2015-04-24 17:02:53 -0700

    Date Time method added

--- src/main/scala/common/parsers/logStringParser.scala
+++ src/main/scala/common/parsers/logStringParser.scala
@@ -1,9 +1,11 @@
 package common.parsers
 
+import java.text.SimpleDateFormat
+
 import com.github.nscala_time.time.Imports._
 
 case class logStringParser(ip: String, clientID: String, userID: String,
-                           timeStamp: String, requestLine: String, statusCode: Int,
+                           timeStamp: DateTime, requestLine: String, statusCode: Int,
                             size: Long, referer: String, browserInfo: String)
 
 object logStringParser {
@@ -14,13 +16,14 @@ object logStringParser {
     val matched = pattern.findFirstMatchIn(log)
     val m = matched.get
 
-    logStringParser(m.group(1), m.group(2),m.group(3),m.group(4),
+    logStringParser(m.group(1), m.group(2),m.group(3), convertToDateTime(m.group(4)),
       m.group(5),m.group(6).toInt, m.group(7).toLong, m.group(8), m.group(9))
   }
 
   def convertToDateTime(timeStamp: String): DateTime = {
-
-
+    val formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+    val timeOfRequest = new DateTime(formatter.parse(timeStamp))
+    timeOfRequest
   }
 
 }
